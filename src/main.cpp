dcdccb21ff5b9b68c06071e1c476ec1e687d612a
Celestine-Lux <162866367+Celestine-Lux@users.noreply.github.com>
2025-08-05 09:20:58 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -22,18 +22,39 @@ static int centerButtonState = 0;
 bool leftButtonHover = false;
 bool centerButtonHover = false;
 bool rightButtonHover = false;
+static bool isPlaying = false;
 static const SDL_FRect rect_left_button = { 50, 280, 25, 25 };
-static const SDL_FRect rect_center_button = { 120, 280, 30, 30 };
+static const SDL_FRect rect_center_button = { 120, 280, 25, 25 };
 static const SDL_FRect rect_right_button = { 190, 280, 25, 25 };
+static float audio_duration_ms = 0.0f;
+static float audio_playback_ms = 0.0f;
 static void render_button(const SDL_FRect *rect, const char *str, int button_value, bool isHover);
 static void SDL_RenderCircle(SDL_Renderer *render, float x, float y, float radius);
 void audio_callback(void* userdata, SDL_AudioStream* stream, int additional_amount, int total_amount);
+void play_mp3(const std::string &filePath);
+
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *args[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "视频初始化失败: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
     if (!SDL_CreateWindowAndRenderer("Nova Stella", 570, 335, SDL_WINDOW_EXTERNAL, &window, &render)) {
         SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "初始化失败: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+    if (SDL_InitSubSystem(SDL_INIT_AUDIO) != 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "音频初始化失败: %s", SDL_GetError());
+    }
+
+    if (SDL_InitSubSystem(SDL_INIT_EVENTS) != 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "事件系统初始化失败: %s", SDL_GetError());
+        SDL_QuitSubSystem(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+        return SDL_APP_FAILURE;
+    }
+
     backgroundTexture = IMG_LoadTexture(render, "image/context.jpg");
     if (!backgroundTexture) {
         SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "加载背景图片失败: %s", SDL_GetError());
@@ -95,22 +116,19 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
             return SDL_APP_SUCCESS; 
         case SDL_EVENT_MOUSE_BUTTON_DOWN: {
             float left_distance = SDL_powf(event->button.x - (rect_left_button.x + rect_left_button.w / 2), 2) +
-                                 SDL_powf(event->button.y - (rect_left_button.y + rect_left_button.h / 2), 2);
+                                SDL_powf(event->button.y - (rect_left_button.y + rect_left_button.h / 2), 2);
             float center_distance = SDL_powf(event->button.x - (rect_center_button.x + rect_center_button.w / 2), 2) +
-                                   SDL_powf(event->button.y - (rect_center_button.y + rect_center_button.h / 2), 2);
+                                SDL_powf(event->button.y - (rect_center_button.y + rect_center_button.h / 2), 2);
             float right_distance = SDL_powf(event->button.x - (rect_right_button.x + rect_right_button.w / 2), 2) +
-                                  SDL_powf(event->button.y - (rect_right_button.y + rect_right_button.h / 2), 2);
-#ifdef DEBUG
-            if (left_distance <= SDL_powf(rect_left_button.w / 2, 2)) {
-                SDL_Log("[SDL_Log]点击了左按钮");
-            } else if (center_distance <= SDL_powf(rect_center_button.w / 2, 2)) {
-                SDL_Log("[SDL_Log]点击了中按钮");
-                centerButtonState = !centerButtonState;
-            } else if (right_distance <= SDL_powf(rect_right_button.w / 2, 2)) {
-                SDL_Log("[SDL_Log]点击了右按钮");
-            }
-#endif
-            break; 
+                                SDL_powf(event->button.y - (rect_right_button.y + rect_right_button.h / 2), 2);
+            if (center_distance <= SDL_powf(rect_center_button.w / 2, 2)) {
+                centerButtonState = (centerButtonState + 1) % 2;
+                if (!isPlaying) {
+                    play_mp3("audio/music.mp3");
+                    isPlaying = true;
+                }
+            }   
+            break;
         }
         case SDL_EVENT_MOUSE_MOTION: {
             float left_distance = SDL_powf(event->motion.x - (rect_left_button.x + rect_left_button.w / 2), 2) +
@@ -152,12 +170,28 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
     SDL_RenderClear(render);
     SDL_SetRenderTarget(render, target);
     
-    SDL_FRect barRect = { 0, 255, 570, 80 };
+    SDL_FRect barRect = { 0, 273, 570, 40 };
     SDL_RenderTexture(render, barTexture, NULL, &barRect);
 
     render_button(&rect_left_button, "LEFT", -1, leftButtonHover);
     render_button(&rect_center_button, "CENTER", 0, centerButtonHover);
     render_button(&rect_right_button, "RIGHT", 1, rightButtonHover);
+
+    if (isPlaying && audio_duration_ms > 0) {
+        float progress = audio_playback_ms * 1.0f / audio_duration_ms;
+        SDL_FRect progressBgRect = { 50, 320, 470, 5 };
+        SDL_SetRenderDrawColor(render, 100, 100, 100, 255);
+        SDL_RenderFillRect(render, &progressBgRect);
+        SDL_FRect progressRect = { 50, 320, 470 * progress, 5};
+        SDL_SetRenderDrawColor(render, 255, 182, 193, 255);
+        SDL_RenderFillRect(render, &progressRect);
+        if (isPlaying && audio_playback_ms >= audio_duration_ms) {
+            isPlaying = false;
+            centerButtonState = 0;
+            progressRect.w = 470;
+            SDL_RenderFillRect(render, &progressRect);
+        }
+    }
 
     SDL_RenderPresent(render);
     return SDL_APP_CONTINUE;
@@ -189,6 +223,7 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     SDL_Quit();
 }
 
+
 static void render_button(const SDL_FRect *rect, const char *str, int button_value, bool isHover) {
     SDL_Texture* buttonTexture = NULL;
     switch(button_value) {
@@ -196,7 +231,7 @@ static void render_button(const SDL_FRect *rect, const char *str, int button_val
         case 0: 
             if (centerButtonState == 0) {
                 buttonTexture = centerButtonTexture;
-            } else {
+            }else {
                 buttonTexture = centerButtonTexture2;
             }
             break;
