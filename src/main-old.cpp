ddc68484e85681f23fe32fb24dbb41f67818a364
commence1 <2364045784@qq.com>
2025-08-07 17:28:38 +0800

    feat: add Vulkan support and update dependencies in CMake and vcpkg configuration

--- src/main-old.cpp
+++ src/main-old.cpp
@@ -1,5 +1,7 @@
 #define SDL_MAIN_USE_CALLBACKS
 
+#include <vulkan/vulkan.hpp>
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3_image/SDL_image.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #define SDL_MAIN_USE_CALLBACKS
 
+#include <vulkan/vulkan.hpp>
+
+#include<glm/glm.hpp>
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
@@ -12,6 +16,14 @@ SDL_Renderer *render;
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **args) {
     if (!SDL_CreateWindowAndRenderer("Nova Stella", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_EXTERNAL | SDL_WINDOW_RESIZABLE, &window, &render)) {
         SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to initialize: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    if (!window || !render) {
+        SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to create window or renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    if (!SDL_InitSubSystem(SDL_INIT_AUDIO)) {
+        SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to initialize audio: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     return SDL_APP_CONTINUE;
